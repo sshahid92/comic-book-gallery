8721be27bd402294c4d5e1322558a86d6ff2e56b
Shah Shahid <shahshahid@quickenloans.com>
2018-11-26 09:48:07 -0500

    added ComicBook view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            return View();
+        }
     }
 }
